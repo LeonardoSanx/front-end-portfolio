1b8e99ed359e86029a5ab7ed90c9a21725af6adf
LeonardoSanx <leonardosanmac@gmail.com>
2021-11-03 00:26:36 -0300

    only portfolio

--- portfolio/components/styledComponents.js
+++ portfolio/components/styledComponents.js
@@ -3,4 +3,8 @@ import styled from "styled-components";
 export const Section = styled.section`
   padding: 80px 10% 0 10%;
   width: 100%;
+
+  @media (max-width: 650px) {
+    padding: 80px 8% 0 8%;
+  }
 `;
--- weather-app/components/Background.js
+++
@@ -1,8 +0,0 @@
-// import BackgroundStyles from "../styles/Background.module.css";
-import { useState, useEffect } from "react";
-
-const Background = ({ day, imageText }) => {
-  return <img src={`images/${day}/${imageText}.jpg`} />;
-};
-
-export default Background;
